6f01c5d7240fe8f6f035ba059ebcf2294c403572
simo255 <bmohamed255@gmail.com>
2020-11-11 17:49:00 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -25,7 +25,7 @@ def find_closest_index_np(values: np.ndarray, number: float) -> int:
 
 def courbe():
     x = np.linspace(-1, 1, 250)
-    y = x**2 * np.sin⁡(1/(x**2))+x
+    y = x**2 * np.sin(1/x**2)
 
     return plt.plot(x,y)
 
